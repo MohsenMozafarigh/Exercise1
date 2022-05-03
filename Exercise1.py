6eefd72f68ffb477be17e789faaa70938106139b
Mohsen <68563395+MohsenMozafarigh@users.noreply.github.com>
2022-05-03 19:39:06 -0400

    Add files via upload

---
+++ Exercise1.py
@@ -0,0 +1,12 @@
+a= input("please add a numbers")
+b = input("please add a numbers")
+a = int(a)
+b = int(b)
+def exercise1(a, b):
+    output1 = a - b
+    output2 = a + b
+    output3 = a * b
+    output4 = a ** b
+    output5 = a / b
+    print(output1, output2 , output3, output4, output5)
+exercise1(a, b)
