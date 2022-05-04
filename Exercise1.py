087221f1987a3da1e093cc7c0b95a061a9764096
BahareCh <94406774+BahareCh@users.noreply.github.com>
2022-05-03 20:35:17 -0400

    Update Exercise1.py

--- Exercise1.py
+++ Exercise1.py
@@ -1,12 +1,14 @@
 a= input("please add a number")
 b = input("please add a number")
+c = input("please add a number")
 a = int(a)
 b = int(b)
-def exercise1(a, b):
-    output1 = a - b
-    output2 = a + b
-    output3 = a * b
-    output4 = a ** b
-    output5 = a / b
+c = int(c)
+def exercise1(a, b, c):
+    output1 = a - b -c
+    output2 = a + b + b
+    output3 = a * b * c
+    output4 = a ** b ** c
+    output5 = a / b /c
     print(output1, output2 , output3, output4, output5)
-exercise1(a, b)
+exercise1(a, b, c)
